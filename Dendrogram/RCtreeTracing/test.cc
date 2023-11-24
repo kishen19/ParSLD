5e699401418e3cf7792b907e6a83c6fb5dcc992d
Laxman Dhulipala <laxmandhulipala@gmail.com>
2023-11-24 15:54:08 -0500

    add exp

--- Dendrogram/RCtreeTracing/test.cc
+++ Dendrogram/RCtreeTracing/test.cc
@@ -16,32 +16,60 @@ namespace {
 }   // namespace gbbs
 
 
+void run_t1() {
+  size_t n = 100000000;
+  using uintE = gbbs::uintE;
+
+  using K=std::pair<uintE, uintE>;
+  using V = uintE;
+
+  std::tuple<K, V> empty = std::make_tuple(std::make_pair(UINT_E_MAX, UINT_E_MAX), 0);
+  auto hash_func = [] (K k) -> size_t {
+    size_t u, v;
+    std::tie(u,v) = k;
+    return parlay::hash64((static_cast<size_t>(u) << 32UL) | static_cast<size_t>(v));
+  };
+  gbbs::timer t; t.start();
+  auto table = gbbs::make_sparse_table<K, V>(n, empty, hash_func);
+  t.next("Init time");
+  parlay::parallel_for(0, n/4, [&] (size_t i) {
+    K k = {i, i+1};
+    V v = 0;
+    table.insert(std::make_tuple(k,v));
+  });
+  t.next("Insert time");
+}
+
+void run_t2() {
+  size_t n = 100000000;
+  using uintE = gbbs::uintE;
+
+  using K=size_t;
+  using V = uintE;
+
+  std::tuple<K, V> empty = std::make_tuple(UINT_E_MAX, 0);
+  auto hash_func = [] (K k) -> size_t {
+    return parlay::hash64(k);
+  };
+  gbbs::timer t; t.start();
+  auto table = gbbs::make_sparse_table<K, V>(n, empty, hash_func);
+  t.next("Init time");
+  parlay::parallel_for(0, n/4, [&] (size_t i) {
+    K k = (i << 32UL) | (i+1); 
+    V v = 0;
+    table.insert(std::make_tuple(k,v));
+  });
+  t.next("Insert time");
+}
+
+
 int main() {
 
   std::cout << "here!" << std::endl;
-  size_t n = 100000000;
 
-  using uintE = gbbs::uintE;
-    using K=std::pair<uintE, uintE>;
-    using V = uintE;
+  run_t1();
 
-    std::tuple<K, V> empty = std::make_tuple(std::make_pair(UINT_E_MAX, UINT_E_MAX), 0);
-    auto hash_func = [] (K k) -> size_t {
-      size_t u, v;
-      std::tie(u,v) = k;
-      return parlay::hash64((static_cast<size_t>(u) << 32UL) | static_cast<size_t>(v));
-    };
-    gbbs::timer t; t.start();
-    auto table = gbbs::make_sparse_table<K, V>(n, empty, hash_func);
-    t.next("Init time");
-    parlay::parallel_for(0, n/4, [&] (size_t i) {
-      K k = {i, i+1};
-      V v = 0;
-      table.insert(std::make_tuple(k,v));
-    });
-    t.next("Insert time");
-
-
+  run_t2();
 
   return 1;
 }
